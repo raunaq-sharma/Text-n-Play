54c204e0f3e11e23b7f17f456e2102de99badbbf
raunaq-sharma <62087497+raunaq-sharma@users.noreply.github.com>
2020-07-13 21:34:47 -0500

    Add files via upload

--- NewGame.py
+++ NewGame.py
@@ -4,8 +4,8 @@ from random import randint
 class Scene(object):
     
     def start(self):
-        print ("The subclasses will inherit and implement start().")
-            exit(1)
+        return "The subclasses will inherit and implement start()."
+        exit(1)
             
 class Engine(object):
     
@@ -13,34 +13,33 @@ class Engine(object):
         self.mapping = mapping
         
     def play(self):
-        current = self.mapping.opening()
+        current = self.mapping.opening_scene()
         
         while True:
-            print ("\n-------")
+            return ("\n-------")
             next_scene = current.start()
             current = self.mapping.next_scene(next_scene)
             
 class Death(Scene):
     
-    display = "You have died..."
+    display = "Wasted! You have died..."
     
     def start(self):
-        print Death.display
+        return Death.display
         exit(1)
         
 class Entrance(Scene):
 
     def start(self, action):
-        data = "You have entered the cave. The path before you bifurcates.\n Go left or right?"
-        return data
+        opening = "You have entered the cave. The path before you bifurcates.\n Go left or right?"
         
-        if action == "left"
+        if action == "left":
             data = "You fall into a deep chasm. Good job..."
             return data, 'death'
             
-        elif action == "right"
+        elif action == "right":
             data = "You venture deeper into the cave."
-            return data, 'lower_level"
+            return opening, data, 'lower_level'
             
         else:
             data = "Unable to comply."
@@ -49,9 +48,8 @@ class Entrance(Scene):
 class LowerLevel(Scene):
 
     def start(self, unlock):
-        data = "You reach a locked door with 5 buttons on it. Which one do you press?"
+        opening = "You reach a locked door with 5 buttons on it. Which one do you press?"
         button = f"{randint(1,5)}"
-        return data
         
         if unlock == button:
             data = "Gate unlocked! You venture forth..."
@@ -66,4 +64,43 @@ class LowerLevel(Scene):
             return data, 'lowerlevel'
             
 class TreasureRoom(Scene):
+   
+   def start(self, decision):
+        opening = "A treasure chest lies in front of you, and an exit to the left. Try opening it, if your chances are more than 50%, it will open."
+        chance = randint(1,60)
+        
+        if decision == "open" and chance > 50:
+            data = f"Your chance is {chance}. Opened!"
+            return data, 'finished'
+            
+        elif decision == "open" and chance < 50:
+            data = f"Your chance is {chance}. Oops! Better luck next time..."
+            return data, 'death'
+        else:
+            data = "Unable to comply."
+            return data, 'treasureroom'
+            
+class Map(object):
+
+    scenes = {
+        'entrance': Entrance(),
+        'lowerlevel': LowerLevel(),
+        'treasureroom': TreasureRoom(),
+        'death': Death()
+        }
     
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+    def next_scene(self, scene_name):
+        return Map.scenes.get(scene_name)
+        
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
+    
+def main():
+    start_map = Map('entrance')
+    start_game = Engine(start_map)
+    start_game.play()
+    
+main()
